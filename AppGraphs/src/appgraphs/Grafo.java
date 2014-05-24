2e8e8bc0975ed0fd3486dddb5016eb4f2d5dc023
Rafael Lopez <rafael_lopez@ucol.mx>
2014-05-24 12:42:20 -0500

    Agregar ruta (formar posiciones de grafo)

---
+++ AppGraphs/src/appgraphs/Grafo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appgraphs;
+
+import java.util.List;
+
+/**
+ *
+ * @author Rflpz
+ */
+public class Grafo {
+    //Atributos
+    List<Nodo>  listos=null;
+    char[]  nodos; //Nodo identifier
+    int distanciaRutaMasCorta = Integer.MAX_VALUE; //Distancia de la ruta mas corta
+    String  rutaMasCorta;  //Ruta mas corta encontrada
+    int[][] grafo; //Matriz de las distancias
+    
+/*-----------------------------------------------------*/    
+    //Constructor
+    Grafo(String serieNodos) {
+        nodos = serieNodos.toCharArray();
+        grafo = new int[nodos.length][nodos.length];
+    }
+/*-----------------------------------------------------*/
+    //Agregar ruta
+    public void agregarRuta(char origen, char destino, int distancia) {
+        int n1 = posicionNodo(origen);
+        int n2 = posicionNodo(destino);
+        grafo[n1][n2]=distancia;
+        grafo[n2][n1]=distancia;
+    }
+
+}
